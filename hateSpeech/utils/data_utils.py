0f7b7df46d64912ee410c230eb0d8d2c658d209c
Tanmay Patel <tanmaykpatel03@gmail.com>
2026-02-12 07:30:10 +0100

    Update version of the data from v1 to v2

--- hateSpeech/utils/data_utils.py
+++ hateSpeech/utils/data_utils.py
@@ -31,7 +31,7 @@ def initialize_dvc_storage(dvc_remote_name: str, dvc_remote_url: str) -> None:
         DATA_UTILS_LOGGER.info("DVC remote storage is already configured.")
 
 def commit_to_dvc(dvc_raw_data_folder: str, dvc_remote_name: str) -> None:
-    current_version = ""
+    current_version = run_shell_command("git tag --list | sort -t v -k 2 -g | tail -1 | sed 's/v//'").strip()
     if not current_version:
         current_version = "0"
 
